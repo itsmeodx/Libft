c522fbee9856a146c0339c4034a5d57ea54d677f
itsmeodx <oussama15ouaadich@gmail.com>
2024-09-18 12:55:55 +0100

    Fix memory allocation and termination in ft_strdup.c

--- src/ft_strdup.c
+++ src/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: oouaadic <oouaadic@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 18:22:01 by oouaadic          #+#    #+#             */
-/*   Updated: 2024/04/27 16:38:38 by oouaadic         ###   ########.fr       */
+/*   Updated: 2024/09/18 12:55:37 by oouaadic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ char	*ft_strdup(const char *s)
 	len = 0;
 	while (s[len])
 		len++;
-	dup_str = (char *)malloc(len);
+	dup_str = (char *)malloc(len + 1);
 	if (dup_str == NULL)
 		return (NULL);
 	i = -1;
 	while (s[++i])
 		dup_str[i] = s[i];
+	dup_str[i] = '\0';
 	return (dup_str);
 }
