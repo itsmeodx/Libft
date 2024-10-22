3f366f7bd82a985b22f280aed15cb6e03343898e
itsmeodx <oussama15ouaadich@gmail.com>
2024-10-22 17:37:53 +0100

    Fix off-by-one error in ft_strlen.c

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: oouaadic <oouaadic@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 09:52:32 by oouaadic          #+#    #+#             */
-/*   Updated: 2024/09/18 12:08:27 by oouaadic         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:34:36 by oouaadic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	size_t	len;
 
 	len = 0;
-	while (s && s[len])
+	while (s[len])
 		len++;
 	return (len);
 }
